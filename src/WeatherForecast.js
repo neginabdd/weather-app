5d172fe613ee1a778820d7fea41cc211b2c8fb07
Negin <negin15.abd@gmail.com>
2024-09-07 19:12:01 +0330

    first day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,6 @@
 import React,{useState} from "react";
 import WeatherIcon from "./WeatherIcon";
+import WeatherForcastDay from "./WeatherForecastDay";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
@@ -15,22 +16,15 @@ export default function WeatherForecast(props) {
     return (
       <div>
         <div className="grid grid-rows-[auto auto auto] gap-2 mb-5">
-          <div className="WeatherForcast-day">Tu</div>
-          <div>
-            <WeatherIcon code="clear-sky-day" width={55} height={55} />
-          </div>
-          <div className="flex items-center gap-2">
-            <span className="Max-temp">19°</span>
-            <span className="Min-temp opacity-60">10°</span>
-          </div>
+          <WeatherForcastDay data={forecast[0]}/>
         </div>
       </div>
     );
   }
   else{
     const apiKey = "49505e5b19424fcteac5488o13e08d43";
-    let lon = props.coordinates.lon;
-    let lat = props.coordinates.lat;
+    let lon = props.coordinates.longitude;
+    let lat = props.coordinates.latitude;
     const apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
     return null;
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,30 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+export default function WeatherForcastDay(props){
+    function day(){
+        let date = new Date(props.data.time*1000);
+        let day = date.getDay();
+        let days =["Sun","Mon","Tue","Wen","Thu","Fri","Sat"]
+        return days[day];
+    }
+    function maxTemp(){
+        let temp=Math.round(props.data.temperature.maximum);
+        return(`${temp}°`);
+    }
+    function minTemp(){
+        let temp=Math.round(props.data.temperature.minimum);
+        return(`${temp}°`);
+    }
+    return(
+        <div>
+    <div className="WeatherForcast-day">{day()}</div>
+          <div>
+            <WeatherIcon code="clear-sky-day" width={55} height={55} />
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="Max-temp">{maxTemp()}</span>
+            <span className="Min-temp opacity-60">{minTemp()}</span>
+          </div>
+          </div>
+    );
+}
